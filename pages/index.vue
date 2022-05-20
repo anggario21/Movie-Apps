7cae607e2adb524b7833ae51f9007457609bc933
anggario21d <anggario_21@apps.ipb.ac.id>
2022-05-21 00:39:08 +0700

    initial push + header

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Hero />
+  </div>
+</template>
+
+<script>
+import axios from '@nuxtjs/axios'
+export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get()
+    },
+  },
+}
+</script>
